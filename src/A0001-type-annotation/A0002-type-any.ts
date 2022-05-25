8873235bc7b4485a10a25957986694d8773133a6
leosango <souoleo@gmail.com>
2022-05-25 18:19:26 -0300

    feat: type any

---
+++ src/A0001-type-annotation/A0002-type-any.ts
@@ -0,0 +1,7 @@
+// Utilize any apenas em ultimo caso 3
+function showMessage(msg: any) {
+  return msg;
+}
+console.log(showMessage([1, 2, 3]));
+console.log(showMessage('olá'));
+console.log(showMessage(1));
